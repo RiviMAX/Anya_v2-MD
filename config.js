4f91b8c1483cc84091ba776ea1a1ec998d2dd8f9
Rivishan Sandeep <87434970+RiviMAX@users.noreply.github.com>
2023-07-12 23:26:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,22 +22,22 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
+global.botname = "ℜ𝔦𝔳𝔦𝔰𝔥𝔞𝔫-𝔐𝔇" 
+global.author = "ꪶ𝚳𝐑.𝐑𝚰𝛁𝚰𝐒𝚮𝚫𝚴ꫂ"  
+global.packname = "ℜ𝔦𝔳𝔦𝔰𝔥𝔞𝔫-𝔐𝔇"  
+global.myweb = "http://t.me/Rivishan3634"
+global.footer = "©\t" + "𝐑𝐢𝐯𝐢𝐬𝐡𝐚𝐧-𝐌𝐃"
 global.prefa = ['-'] // single prefix
 global.themeemoji = "🎐"
-global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
-global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
+global.socialText = `*⪧⪢ Contact Bot Owner.*`
+global.socialLink = `*⪧⪢ http://wa.me/94789864930*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "ꪶ𝚳𝐑.𝐑𝚰𝛁𝚰𝐒𝚮𝚫𝚴ꫂ"    
+global.ownernumber = ["94789864930"]
+global.ytname = "YT: Voiceland Karaoke"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
+global.region = "Sri Lanka"
 global.state = "Enter Your State Name Here" // your state name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
